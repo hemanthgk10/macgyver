e16b9c2ad990644d39e7d9666a8e9ebd0d2853cc
Rob Schoening <robschoening@gmail.com>
2015-08-29 09:05:14 -0700

    implement #210 add better service factory support

--- macgyver-plugin-cloud-aws/src/main/java/io/macgyver/plugin/cloud/aws/AWSServiceFactory.java
+++ macgyver-plugin-cloud-aws/src/main/java/io/macgyver/plugin/cloud/aws/AWSServiceFactory.java
@@ -11,36 +11,63 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSCredentialsProviderChain;
 import com.amazonaws.auth.BasicAWSCredentials;
-
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.google.common.base.Strings;
 
 public class AWSServiceFactory extends ServiceFactory<AWSServiceClient> {
 
 	public AWSServiceFactory() {
 		super("aws");
-		
+
 	}
 
 	@Override
 	protected AWSServiceClient doCreateInstance(ServiceDefinition def) {
-		
-		final BasicAWSCredentials c = new BasicAWSCredentials(def.getProperties().getProperty("accessKey"), def.getProperties().getProperty("secretKey"));
-		
+
+		AWSServiceClientImpl ci = new AWSServiceClientImpl();
+		ci.credentialsProvider = newProviderChain(def);
+		String regionName = Strings.emptyToNull(Strings.nullToEmpty(
+				def.getProperties().getProperty("region")).trim());
+		if (regionName != null) {
+			logger.info("setting region: {}", regionName);
+			ci.defaultRegion = Region.getRegion(Regions.fromName(regionName));
+		}
+
+		return ci;
+	}
+
+	AWSCredentialsProvider newProviderChain(ServiceDefinition def) {
+
+		final String accessKey = def.getProperties().getProperty("accessKey");
+		final String secretKey = def.getProperties().getProperty("secretKey");
+
 		AWSCredentialsProvider cp = new AWSCredentialsProvider() {
-			
+
 			@Override
 			public void refresh() {
-				
+
 			}
-			
+
 			@Override
 			public AWSCredentials getCredentials() {
-				return c;
+				if (Strings.isNullOrEmpty(accessKey)
+						|| Strings.isNullOrEmpty(secretKey)) {
+					logger.info("accessKey or secretKey not specified.  Falling back to DefaultAWSCredentialsProviderChain.");
+					return null;
+				} else {
+					final BasicAWSCredentials c = new BasicAWSCredentials(def
+							.getProperties().getProperty("accessKey"), def
+							.getProperties().getProperty("secretKey"));
+					return c;
+				}
 			}
 		};
-		
-		AWSServiceClientImpl ci = new AWSServiceClientImpl();
-		ci.credentialsProvider = cp;
-		return ci;
+
+		AWSCredentialsProviderChain chain = new AWSCredentialsProviderChain(cp,
+				new DefaultAWSCredentialsProviderChain());
+		return chain;
 	}
 
 }
