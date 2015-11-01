b61819b890be6337b5fe7549e27b1b8cf3bd5716
Rob Schoening <robschoening@gmail.com>
2015-10-31 17:20:21 -0700

    more tests....excruciating

--- macgyver-plugin-cloud-aws/src/main/java/io/macgyver/plugin/cloud/aws/scanner/EC2InstanceScanner.java
+++ macgyver-plugin-cloud-aws/src/main/java/io/macgyver/plugin/cloud/aws/scanner/EC2InstanceScanner.java
@@ -18,8 +18,10 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.macgyver.core.util.JsonNodes;
 import io.macgyver.neorx.rest.NeoRxClient;
 import io.macgyver.plugin.cloud.aws.AWSServiceClient;
+import joptsimple.internal.Strings;
 
 import com.amazonaws.regions.Region;
 import com.amazonaws.services.ec2.AmazonEC2Client;
@@ -27,7 +29,7 @@ import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 
 import com.fasterxml.jackson.databind.JsonNode;
-
+import com.google.gwt.thirdparty.guava.common.base.Preconditions;
 
 public class EC2InstanceScanner extends AWSServiceScanner {
 
@@ -53,38 +55,52 @@ public class EC2InstanceScanner extends AWSServiceScanner {
 
 	@Override
 	public Optional<String> computeArn(JsonNode n) {
-		
-		return Optional.of(String.format("arn:aws:ec2:%s:%s:instance/%s", n.path("aws_region").asText(),n.path("aws_account").asText(),n.path("aws_instanceId").asText()));
+
+		String region = n.path("aws_region").asText(null);
+		String account = n.path("aws_account").asText(null);
+		String instanceId = n.path("aws_instanceId").asText(null);
+
+		Preconditions.checkState(!Strings.isNullOrEmpty(region), "aws_region not set");
+		Preconditions.checkState(!Strings.isNullOrEmpty(account), "aws_account not set");
+		Preconditions.checkState(!Strings.isNullOrEmpty(instanceId), "aws_instanceId not set");
+
+		return Optional.of(String.format("arn:aws:ec2:%s:%s:instance/%s", region, account, instanceId));
 
 	}
 
 	@Override
 	public void scan(Region region) {
-		try { 
-			AmazonEC2Client client = getAWSServiceClient().createEC2Client(region);
-	
-			DescribeInstancesResult result = client.describeInstances();
-	
-			result.getReservations().forEach(reservation -> {
+
+		AmazonEC2Client client = getAWSServiceClient().createEC2Client(region);
+
+		DescribeInstancesResult result = client.describeInstances();
+
+		result.getReservations().forEach(reservation -> {
+
+			reservation.getInstances().forEach(instance -> {
+
+				JsonNode n = convertAwsObject(instance, region);
+
+				String subnetId = n.path("aws_subnetId").asText(null);
+				String arn = n.path("aws_arn").asText(null);
+				String account = n.path("aws_account").asText(null);
+				Preconditions.checkState(!Strings.isNullOrEmpty(subnetId),"aws_subnetId must not be null");
+				Preconditions.checkState(!Strings.isNullOrEmpty(arn),"aws_arn must not be null");
+				Preconditions.checkState(!Strings.isNullOrEmpty(account),"aws_account must not be null");
+				// This is technically ambiguous since subnetId's are not guaranteed to be unique across accounts
+				// need to qualify with account.  Or more properly, the computed arn of the subnet
 				
-				reservation.getInstances().forEach(instance -> {
-					
-					JsonNode n = convertAwsObject(instance, region);
-	
-					String cypher = "match (x:AwsSubnet {aws_subnetId:{aws_subnetId}}) "
-							+ "merge (y:AwsEc2Instance {aws_arn:{arn}}) set y+={props}, y.updateTs=timestamp() "
-							+ "merge (y)-[:RESIDES_IN]->(x)";
-					
-					NeoRxClient neoRx = getNeoRxClient();
-					neoRx.execCypher(cypher, "aws_subnetId",n.path("aws_subnetId").asText(), "arn",n.path("aws_arn").asText(),"props",n);
-					
-	
-				});
-				
+				String cypher = "match (x:AwsSubnet {aws_subnetId:{aws_subnetId}, aws_account:{aws_account}}) "
+						+ "merge (y:AwsEc2Instance {aws_arn:{arn}}) set y+={props}, y.updateTs=timestamp() "
+						+ "merge (y)-[:RESIDES_IN]->(x)";
+
+				NeoRxClient neoRx = getNeoRxClient();
+				neoRx.execCypher(cypher, "aws_subnetId", subnetId, "aws_account",account,"arn",
+						arn, "props", n);
+
 			});
-		} catch (RuntimeException e) {
-			logger.warn("problem scanning EC2 instances",e);
-		}
+
+		});
 
 	}
 
