9a039b8e61674179b592b96f1fc681283166f325
Rob Schoening <robschoening@gmail.com>
2016-04-27 06:11:48 -0700

    log source host

--- macgyver-core/src/main/java/io/macgyver/core/log/EventLogger.java
+++ macgyver-core/src/main/java/io/macgyver/core/log/EventLogger.java
@@ -3,7 +3,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,8 @@
  */
 package io.macgyver.core.log;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.Instant;
 import java.util.Date;
 
@@ -24,60 +26,83 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 
 import io.macgyver.core.reactor.MacGyverEventPublisher;
 import io.macgyver.core.reactor.MacGyverMessage;
 
 public final class EventLogger {
 
-	Logger logger = LoggerFactory.getLogger(EventLogger.class);
+	static Logger logger = LoggerFactory.getLogger(EventLogger.class);
 
 	@Autowired
 	MacGyverEventPublisher publisher;
-	
+
 	ObjectMapper mapper = new ObjectMapper();
 
-	
+	public Supplier<String> sourceHostSupplier = Suppliers.memoize(new SourceHostSupplier());
+
+	public static class SourceHostSupplier implements Supplier<String> {
+
+		@Override
+		public String get() {
+			try {
+				String host = InetAddress.getLocalHost().getHostName();
+				return host;
+			} catch (UnknownHostException | RuntimeException e) {
+				logger.warn("could not obtain local host name", e);
+			}
+			return "localhost";
+		}
+
+	}
+
 	public class LogMessage extends MacGyverMessage {
-		String label=null;
-		boolean sent=false;
+		String label = null;
+		boolean sent = false;
+
 		public LogMessage withLabel(String label) {
 			this.label = label;
 			return this;
 		}
+
 		public String getLabel() {
 			return label;
 		}
+
 		public LogMessage withMessage(String msg) {
 			return (LogMessage) withAttribute("message", msg);
 		}
-		
+
+		public LogMessage withSourceHost(String host) {
+			return (LogMessage) withAttribute("sourceHost", host);
+		}
+
 		public void log() {
 			logEvent(this);
 		}
 	}
 
-
-
 	public LogMessage event() {
 		return new LogMessage();
 	}
 
-
 	protected final void logEvent(LogMessage event) {
-		
-		
+
 		try {
-		
+
 			// now log to the distributed system
 			Preconditions.checkNotNull(publisher);
-			Preconditions.checkState(event.sent==false,"event already published");
+			Preconditions.checkState(event.sent == false, "event already published");
+			if (!event.getData().has("sourceHost")) {
+				event.withAttribute("sourceHost", sourceHostSupplier.get());
+			}
 			publisher.createMessage().withMessage(event).publish();
-			event.sent=true;
+			event.sent = true;
 		} catch (RuntimeException e) {
 			logger.warn("could not publish log event", e);
 		}
 	}
-
 
 }
