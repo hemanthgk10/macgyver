1036ea61ce3100439640e6f140b67950737a422d
Rob Schoening <robschoening@gmail.com>
2016-06-04 02:06:26 -0700

    global Jackson ObjectMapper

---
+++ macgyver-core/src/main/java/io/macgyver/core/util/MacGyverObjectMapper.java
@@ -0,0 +1,11 @@
+package io.macgyver.core.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class MacGyverObjectMapper {
+
+	public static final ObjectMapper objectMapper = new ObjectMapper();
+	
+	
+	
+}
