54aba940a1b651b8f621100d05656307816645fd
Rob Schoening <robschoening@gmail.com>
2016-12-29 20:09:22 -0800

    conditional bintray publish

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -6,4 +6,11 @@ WORKSPACE="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
 cd $WORKSPACE
 
-git branch
+BUILD_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "${BUILD_BRANCH}" = "bintray" ]; then
+  DEPLOY_OPTS="uploadArchives"
+fi
+
+
+./gradlew clean check ${DEPLOY_OPTS}
